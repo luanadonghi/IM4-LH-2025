2822b10760a544ae5c5d39544596c2b689142dc0
luanadonghi <luana.donghi@stud.fhgr.ch>
2025-05-13 12:04:09 +0200

    updates protected
    kleine Anpassungen

--- js/protected.js
+++ js/protected.js
@@ -4,8 +4,16 @@ console.log ("protected.js geladen");
 fetch("api/protected.php")
 .then((response) => { response.json() })
 .then((data) => {
-  console.log("Antwort vom Server:\n" + data);
-  alert(data);
+  console.log(data);
+ 
+if (data.status === "error") {
+    window.location.href = "login.html";
+} else {
+    document.getElementById("welcome-message"). 
+    innerHTML = 
+    "Willkommen " + data.username; 
+}
+
 })
 .catch((error) => {
   console.error("Fehler beim Senden:", error);
